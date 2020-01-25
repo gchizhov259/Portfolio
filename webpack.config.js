2b556273c3d3e1bb4a9f5db0da5f1ec38a866437
Grigory Chizhov <chizhovg259@gmail.com>
2020-01-25 21:59:22 +0500

    изменения

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = 'https://gchizhov259.github.io/Portfolio/dist';
+  const publicPath = 'https://gchizhov259.github.io/Portfolio/dist/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
