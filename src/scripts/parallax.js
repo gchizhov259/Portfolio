38197defd5dffba76dd996d7cef94295dba1a8bb
Grigory Chizhov <chizhovg259@gmail.com>
2020-02-21 14:43:11 +0500

    параллакс для планшетов

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -10,7 +10,19 @@ function moveLayaersDependsOnScroll(wScroll) {
   });
 }
 
+// function initParallax() {
 window.addEventListener("scroll", e => {
   const wScroll = window.pageYOffset;
   moveLayaersDependsOnScroll(wScroll);
 });
+// }
+
+// window.onresize = function() {
+//   const wellcome = document.querySelector(".wellcome");
+//   if (document.documentElement.clientWidth > 768) {
+//     initParallax();
+//   } else {
+//     wellcome.style.backgroundImage =
+//       "url('../images/content/background/768.jpg')";
+//   }
+// };
